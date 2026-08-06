1479a4d1647904d138054425a035fe61254b904d
Krishna Sahith Boreda <44928471+kvb160230@users.noreply.github.com>
2026-08-06 05:45:43 +0000

    Sync LeetCode submission Runtime - 49 ms (20.28%), Memory - 54 MB (16.82%)

---
+++ solutions/2732-counter/solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    var counter = 0;
+    return function() {
+
+        if (counter == 0) {
+            counter++;
+            return n;
+        }
+        else {
+            n++;
+            return n;
+        }
+    };
+
+};
+
+
+ const counter = createCounter(10)
+ counter() // 10
+ //counter() // 11
+ //counter() // 12
+
