beb67da3978755278dd8b9eb4ea2b2a2eeba5ea1
Krishna Sahith Boreda <44928471+kvb160230@users.noreply.github.com>
2026-08-11 09:47:53 +0000

    Sync LeetCode submission Runtime - 43 ms (61.31%), Memory - 53.5 MB (58.23%)

---
+++ solutions/2746-filter-elements-from-array/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    var newArray = [];
+
+    for(i=0; i<arr.length; i++) {
+        if (fn(arr[i], i)) {
+            newArray.push(arr[i]);
+        }
+    }
+
+    return newArray;
+    
+};
